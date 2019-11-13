9e1668dc4536654420bbc4415e4b8e92968adc96
citizenzet <exgamer@live.ru>
2019-11-13 17:53:41 +0600

    d

--- src/services/StaticPageService.php
+++ src/services/StaticPageService.php
@@ -1,18 +1,20 @@
 <?php
-namespace concepture\yii2static\services;
+namespace concepture\yii2banner\services;
 
 use concepture\yii2logic\forms\Form;
 use concepture\yii2logic\services\Service;
 use Yii;
 use concepture\yii2logic\services\traits\StatusTrait;
 use concepture\yii2logic\services\traits\LocalizedReadTrait;
+use yii\db\ActiveQuery;
+use concepture\yii2logic\enum\StatusEnum;
 
 /**
- * Class StaticPageService
- * @package concepture\yii2static\service
+ * Class BannerService
+ * @package concepture\yii2banner\service
  * @author Olzhas Kulzhambekov <exgamer@live.ru>
  */
-class StaticPageService extends Service
+class BannerService extends Service
 {
     use StatusTrait;
     use LocalizedReadTrait;
@@ -23,20 +25,18 @@ class StaticPageService extends Service
     }
 
     /**
-     * Возвращает статическую страницу для текущего url по хешу md5 url
+     * Возвращает баннеры для текущего url по хешу md5 url
      *
      * @return array
      */
-    public function getPageCurrentUrl()
+    public function getBannersForCurrentUrl()
     {
         $current = Yii::$app->getRequest()->getPathInfo();
         $md5 = md5($current);
-        $modelClass = $this->getRelatedModelClass();
-        $modelClass::$search_by_locale_callable = function($q, $localizedAlias) use ($md5) {
-            $q->andWhere(["{$localizedAlias}.url_md5_hash" => $md5]);
-        };
 
-        return $this->getOneByCondition(function(ActiveQuery $query) {
+        return $this->getAllByCondition(function(ActiveQuery $query) use($md5) {
+            $query->innerJoinWith('urlLinks');
+            $query->andWhere("u.url_md5_hash = :url_md5_hash", [':url_md5_hash' => $md5]);
             $query->andWhere("status = :status", [':status' => StatusEnum::ACTIVE]);
         });
     }
